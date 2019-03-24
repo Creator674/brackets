899c228858c8e38c38aeeefb8b2c96fc106cab3f
Creator674 <666anastyalena@mail.ru>
2019-03-24 23:32:33 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (var i = str.length ; i>=0 ; i--){
+    for (var k = 0; k < bracketsConfig.length; k++){
+      var same = bracketsConfig[k].join('');
+      if (str.includes(same)) {
+        str = str.replace(same,'');
+      }
+    }
+  }
+  return str === "";
 }
+
